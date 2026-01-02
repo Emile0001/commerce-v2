ad5e4273246a024ca6432503af9d10e442654bd8
Emile0001 <boteseh@gmail.com>
2026-01-02 08:45:31 +0200

    added pages

---
+++ lib/constants/index.ts
@@ -0,0 +1,6 @@
+export const APP_NAME =
+    process.env.NEXT_PUBLIC_APP_NAME || "Nextjs Commerce v2";
+export const APP_DESCRIPTION =
+    process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Website description";
+export const SERVER_URL =
+    process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
