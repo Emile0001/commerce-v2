9f9503cd1461417e0f98df16b74e9e85f16ba528
Emile0001 <boteseh@gmail.com>
2026-01-10 12:42:06 +0200

    Navbar comp blueprint

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { APP_NAME, APP_DESCRIPTION } from "@/lib/constants";
+import Navbar from "@/components/navbar/Navbar";
+import Container from "@/components/global/Container";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -28,7 +30,8 @@ export default function RootLayout({
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
-                {children}
+                <Navbar />
+                <Container className="py-20">{children}</Container>
             </body>
         </html>
     );
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,4 +1,23 @@
+import Container from "../global/Container";
+import CartButton from "./CartButton";
+import DarkMode from "./DarkMode";
+import LinksDropdown from "./LinksDropdown";
+import Logo from "./Logo";
+import NavSearch from "./NavSearch";
+
 function Navbar() {
-    return <div>Navbar</div>;
+    return (
+        <nav className="border-b">
+            <Container className="flex flex-col flex-wrap py-8 gap-4 sm:flex-row sm:justify-between sm:items-center ">
+                <Logo />
+                <NavSearch />
+                <div className="flex gap-4 items-center">
+                    <CartButton />
+                    <DarkMode />
+                    <LinksDropdown />
+                </div>
+            </Container>
+        </nav>
+    );
 }
 export default Navbar;
