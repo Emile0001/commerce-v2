4fb3adb560ac205e8f361a18940ef2bd1e68449a
Emile0001 <boteseh@gmail.com>
2026-01-13 12:44:05 +0200

    cartBtn blueprint

--- components/navbar/CartButton.tsx
+++ components/navbar/CartButton.tsx
@@ -1,4 +1,23 @@
-function CartButton() {
-    return <div>CartButton</div>;
+import { FaShoppingCart } from "react-icons/fa";
+import { Button } from "../ui/button";
+import Link from "next/link";
+
+async function CartButton() {
+    const numItemsInCart: number = 9;
+    return (
+        <Button
+            asChild
+            variant={"outline"}
+            size={"icon"}
+            className=" flex justify-center items-center relative"
+        >
+            <Link href={"/cart"}>
+                <FaShoppingCart />
+                <span className="absolute -top-3 -right-3 h-5 w-5 bg-primary text-white rounded-full flex justify-center items-center text-xs">
+                    {numItemsInCart}
+                </span>
+            </Link>
+        </Button>
+    );
 }
 export default CartButton;
