14df687496040cad2645ff91dd09cd2f06da64f8
Emile0001 <boteseh@gmail.com>
2026-01-17 18:56:57 +0200

    format currency function

---
+++ app/utils/actions.ts
@@ -0,0 +1,20 @@
+import db from "@/app/utils/db";
+
+export const fetchFeaturedProducts = async () => {
+    try {
+        return await db.product.findMany({
+            where: { featured: true },
+            orderBy: { createdAt: "desc" },
+        });
+    } catch (error) {
+        console.error("Failed to fetch featured products", error);
+        throw new Error("Could not load featured products");
+    }
+};
+export const fetchAllProducts = async (page = 1, pageSize = 20) => {
+    return db.product.findMany({
+        skip: (page - 1) * pageSize,
+        take: pageSize,
+        orderBy: { createdAt: "desc" },
+    });
+};
--- app/utils/db.ts
+++ app/utils/db.ts
@@ -1,25 +1,45 @@
-import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "pg";
+// import { PrismaClient } from "@prisma/client";
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import { Pool } from "pg";
 
-const pool = new Pool({
+// const pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+// });
+
+// const adapter = new PrismaPg(pool);
+
+// const globalForPrisma = globalThis as unknown as {
+//     prisma: PrismaClient | undefined;
+// };
+
+// const db =
+//     globalForPrisma.prisma ??
+//     new PrismaClient({
+//         adapter,
+//     });
+
+// if (process.env.NODE_ENV !== "production") {
+//     globalForPrisma.prisma = db;
+// }
+
+// export default db;
+import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const globalForPrisma = global as unknown as {
+    prisma: PrismaClient;
+};
+
+const adapter = new PrismaPg({
     connectionString: process.env.DATABASE_URL,
 });
 
-const adapter = new PrismaPg(pool);
-
-const globalForPrisma = globalThis as unknown as {
-    prisma: PrismaClient | undefined;
-};
-
-const db =
-    globalForPrisma.prisma ??
+const prisma =
+    globalForPrisma.prisma ||
     new PrismaClient({
         adapter,
     });
 
-if (process.env.NODE_ENV !== "production") {
-    globalForPrisma.prisma = db;
-}
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
-export default db;
+export default prisma;
