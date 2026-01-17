f356e6a5e82f7f6ff95bb61d46a1574e53b37e96
Emile0001 <boteseh@gmail.com>
2026-01-17 14:23:15 +0200

    seed db

--- app/utils/db.ts
+++ app/utils/db.ts
@@ -1,29 +1,3 @@
-// import { PrismaClient } from "@/app/generated/prisma/client";
-// import { PrismaPg } from "@prisma/adapter-pg";
-// // import { Pool } from "pg";
-
-// // const pool = new Pool({
-// //     connectionString: process.env.DATABASE_URL,
-// // });
-
-// const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-
-// const globalForPrisma = globalThis as unknown as {
-//     prisma: PrismaClient | undefined;
-// };
-
-// const db =
-//     globalForPrisma.prisma ??
-//     new PrismaClient({
-//         adapter,
-//     });
-
-// if (process.env.NODE_ENV !== "production") {
-//     globalForPrisma.prisma = db;
-// }
-
-// export const prisma = new PrismaClient({ adapter });
-// export default db;
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
