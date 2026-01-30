af60dc4339af9564f9ec7252b6b8b3ed4ae334e5
Emile0001 <boteseh@gmail.com>
2026-01-30 19:17:13 +0200

    Breadcrumb component

--- components/single-product/BreadCrumbs.tsx
+++ components/single-product/BreadCrumbs.tsx
@@ -1,4 +1,39 @@
-const BreadCrumbs = () => {
-    return <div>BreadCrumbs</div>;
+import { HomeIcon, ShirtIcon } from "lucide-react";
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "../ui/breadcrumb";
+
+const BreadCrumbs = ({ name }: { name: string }) => {
+    return (
+        <Breadcrumb>
+            <BreadcrumbList>
+                <BreadcrumbItem>
+                    <BreadcrumbLink href="/">
+                        <HomeIcon />
+                    </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                    <BreadcrumbLink href="/products">
+                        <ShirtIcon />
+                        Products
+                    </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                    <BreadcrumbLink>
+                        <BreadcrumbPage className="capitalize text-lg">
+                            {name}
+                        </BreadcrumbPage>
+                    </BreadcrumbLink>
+                </BreadcrumbItem>
+            </BreadcrumbList>
+        </Breadcrumb>
+    );
 };
 export default BreadCrumbs;
