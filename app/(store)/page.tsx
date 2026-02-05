94dd4fb9f54ca8c29fbb9e536ea90c3fb21d6945
Emile0001 <boteseh@gmail.com>
2026-02-05 19:30:22 +0200

    folder restructure

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,1 +1,18 @@
-//Homepage
+import LoadingContainer from "@/components/global/LoadingContainer";
+import FeaturedProducts from "@/components/home/FeaturedProducts";
+import Hero from "@/components/home/Hero";
+
+import { Suspense } from "react";
+
+function HomePage() {
+    return (
+        <>
+            <Hero />
+
+            <Suspense fallback={<LoadingContainer />}>
+                <FeaturedProducts />
+            </Suspense>
+        </>
+    );
+}
+export default HomePage;
