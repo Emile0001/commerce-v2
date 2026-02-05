61c3f3d2fcaf81f2b65f89fb8faef6af33b1c065
Emile0001 <boteseh@gmail.com>
2026-02-05 14:06:28 +0200

    auth and root folder structure

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import FeaturedProducts from "@/components/home/FeaturedProducts";
 import Hero from "@/components/home/Hero";
 
 import { Suspense } from "react";
-import ProductDetails from "./(root)/product/[slug]/page";
+
 function HomePage() {
     return (
         <>
             <Hero />
-            <ProductDetails />
+
             <Suspense fallback={<LoadingContainer />}>
                 <FeaturedProducts />
             </Suspense>
---
+++ app/productDetailsTemp/page.tsx
@@ -0,0 +1,10 @@
+import ProductDetails from "../(root)/product/[slug]/page";
+
+const ProductDetailsTempPage = () => {
+    return (
+        <div>
+            <ProductDetails />
+        </div>
+    );
+};
+export default ProductDetailsTempPage;
