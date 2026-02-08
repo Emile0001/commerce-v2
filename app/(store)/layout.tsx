9ba8e8644527452f953922d825205797a4659eec
Emile0001 <boteseh@gmail.com>
2026-02-08 14:20:26 +0200

    font change and folder updates

--- app/(store)/layout.tsx
+++ app/(store)/layout.tsx
@@ -1,20 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "@/app/globals.css";
 import { APP_NAME, APP_DESCRIPTION } from "@/lib/constants";
 
 import PageContainer from "@/components/layout/PageContainer";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import NavbarMega from "@/components/navbar/NavbarMega";
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
     title: `${APP_NAME}`,
@@ -28,9 +20,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" suppressHydrationWarning>
-            <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-            >
+            <body className={`${inter.className} antialiased`}>
                 <ThemeProvider
                     attribute="class"
                     defaultTheme="system"
