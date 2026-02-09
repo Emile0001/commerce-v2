8b8ab4eb4446be2f5bac03105a1d89a4b4974f0c
Emile0001 <boteseh@gmail.com>
2026-02-09 17:53:13 +0200

    page container and nav refactor classes

--- components/layout/PageContainer.tsx
+++ components/layout/PageContainer.tsx
@@ -8,7 +8,12 @@ function PageContainer({
     className?: string;
 }) {
     return (
-        <div className={cn("mx-auto max-w-6xl xl:max-w-7xl px-8", className)}>
+        <div
+            className={cn(
+                "mx-auto w-full max-w-6xl px-4 sm:px-6 lg:px-8",
+                className,
+            )}
+        >
             {children}
         </div>
     );
--- components/navbar/NavbarMega.tsx
+++ components/navbar/NavbarMega.tsx
@@ -21,14 +21,12 @@ export default function NavbarMega({
     return (
         <header
             className={cn(
-                "border-b bg-background/80 backdrop-blur sticky top-0 z-50",
+                "border-b bg-background/80 backdrop-blur sticky top-0 z-50 mx-auto w-full max-w-7xl 2xl:max-w-350 px-4 sm:px-6 lg:px-8",
                 className,
             )}
         >
-            <PageContainer>
-                <DesktopNav logo={logo} menu={menu} />
-                <MobileNav logo={logo} menu={menu} auth={auth} />
-            </PageContainer>
+            <DesktopNav logo={logo} menu={menu} />
+            <MobileNav logo={logo} menu={menu} auth={auth} />
         </header>
     );
 }
